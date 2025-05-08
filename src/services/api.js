898ee6711000c26d8201b733824b230b4c93fba7
tenthin <tenthinten@gmail.com>
2025-05-08 16:28:59 -0400

    Initial commit

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "1be714db19bda71955d4f70449a4791c";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results;
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+    )}`);
+    const data = await response.json()
+    return data.results;
+}
